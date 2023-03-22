0b59f76d0857db96a79454961f5f046d205ec905
Philipp Winkler <winkler@webit.de>
2023-03-23 00:16:25 +0100

    feat: add vue3 user_interface setup with graphql connection

---
+++ user_interface/src/plugins/graphql/client.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: `${import.meta.env.VITE_API_URL}/graphql`,
+})
+
+export const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
